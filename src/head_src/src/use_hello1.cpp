9a7d9b9667e27d1a40e15a26e0af2f7f36478b68
wb <1440411316@qq.com>
2022-07-30 11:05:15 +0800

    修改use_hello1.cpp

--- src/head_src/src/use_hello1.cpp
+++ src/head_src/src/use_hello1.cpp
@@ -1,13 +1,13 @@
 
 #include "ros/ros.h"
 #include "head_src/hello1.h"
-
+#include<iostream>
 
 
 int main(int argc, char * argv[])
 {
     setlocale(LC_ALL,"");
-    ros::init(argc, argv, "hello_head_src");
+    ros::init(argc, argv, "hello_head_src, hello git branch");
     hello_ns::Myhello Myhello;
     Myhello.run();
 
